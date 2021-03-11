6daaadd2280c9fe1d82c144051d61c6b55f06c7e
Dotsworthy <dotsworthy@gmail.com>
2021-03-11 17:09:46 +0000

    added preview fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,8 +14,8 @@ const IndexPage = () => {
       <section className="content-box" id="yellow">
 
         <div className="pitch-box">
-          <div className="tag1"><h1 >EDUCATION DONE - ALL IN ONE.</h1></div>
-          <div className="tag2"><h1>THE HISTORY OF BRITAIN IS COMPLEX AND DIVERSE - AND OUR EDUCATION SYSTEM SHOULD REFLECT THIS.</h1></div>
+          <div className="tag1"><h1 >Education Done. All in One.</h1></div>
+          <div className="tag2"><h1>The history of Britain is complex and diverse - and our education system should reflect this.</h1></div>
         </div>
 
         <div className="paragraph-box" id="white">
@@ -30,12 +30,12 @@ const IndexPage = () => {
 
 
       <section className="content-box" id="white">
-        <h1>WHAT WE DO</h1>
+        <h1>What we do</h1>
 
         <div className="content-overlay-box">
           <StaticImage src="../images/42401748_m.jpg" alt="enhance-learning" />
           <div className="paragraph-box paragraph-right" id="pink">
-            <h1>ENHANCE LEARNING, FOR LIFE IN MODERN BRITAIN</h1>
+            <h1>Enhance learning, for life in modern Britain</h1>
             <p>By enabling our nations’ educators to teach the intersectional realities of British History through engaging, representative, and accessible materials, we help equip young learners to acknowledge and address multiple perspectives across education and society as a whole, creating exceptional critical thinkers and future global leaders.</p>
           </div>
         </div>
@@ -43,7 +43,7 @@ const IndexPage = () => {
         <div className="content-overlay-box">
           <StaticImage className="img-right" src="../images/96254573_m.jpg" alt="provide resources" />
           <div className="paragraph-box paragraph-left" id="green">
-            <h1>PROVIDE EASY TO USE RESOURCES, FROM GRASSROOTS SOURCES</h1>
+            <h1>Provide easy to use resources, from grassroots sources</h1>
             <p>The All in One resource gateway contains outstanding History lesson materials designed to correspond to and integrate seamlessly into the existing curriculum and examination requirements for every UK nation. All resource materials are made in partnership with existing expert organisations, academics, and UK teachers working currently.</p>
           </div>
 
@@ -52,7 +52,7 @@ const IndexPage = () => {
         <div className="content-overlay-box">
           <StaticImage src={"../images/127784687_m.jpg"} alt="support-change" />
           <div className="paragraph-box paragraph-right" id="yellow">
-            <h1>SUPPORT LASTING CHANGE </h1>
+            <h1>Support lasting change</h1>
             <p>Our approach to educaton reform is two-pronged, encompassing both content creation, and reform lobbying of UK governments to include All In One as a recommended teaching aid for university and college PGCE/PDGE courses, so that a greater consistency in inclusive teaching methods can be achieved across schools/educators.</p>
             
             <div className="link-container">
